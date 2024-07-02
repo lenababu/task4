689e7f02e375206dd380251e930dc31c9e8f364c
lenababu <145546339+lenababu@users.noreply.github.com>
2024-07-02 22:33:08 +0530

    Add files via upload

---
+++ task4.dart
@@ -0,0 +1,9 @@
+void main()
+{
+    String name="Lena Babu";
+    String place="Thrissur";
+    int phno=929839484;
+    print('name:$name');
+    print('place:$place');
+    print('phno:$phno');
+}
